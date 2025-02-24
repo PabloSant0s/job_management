58d3b768d3ed050a6bf177e78e7ce1ebc7fcb930
Pablo Santos <120131370+PabloSant0s@users.noreply.github.com>
2025-02-24 16:04:35 -0300

    feat: criando a entidade company

--- src/main/java/br/com/rocketseat/job_management/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rocketseat/job_management/modules/candidate/CandidateEntity.java
@@ -11,14 +11,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
-@Entity
-@Table(name = "tb_cantidate")
+@Entity(name = "tb_cantidate")
 public class CandidateEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
---
+++ src/main/java/br/com/rocketseat/job_management/modules/company/entities/JobEntity.java
@@ -0,0 +1,37 @@
+package br.com.rocketseat.job_management.modules.company.entities;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name="tb_job")
+public class JobEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String level;
+  private String benefits;
+  private String description;
+
+  @ManyToOne()
+  @JoinColumn(name = "company_id", insertable=false, updatable=false)
+  private CompanyEntity company;
+
+  @Column(name = "company_id")
+  private UUID companyId;
+
+  @CreationTimestamp
+  @Column(name = "created_at")
+  private Instant createdAt;
+}
