aae36214b55c0d717d4349f94a61a40d07935d62
Pablo Santos <120131370+PabloSant0s@users.noreply.github.com>
2025-03-10 20:23:15 -0300

    feat: Teste validando se o Job não existe

--- src/test/java/br/com/rocketseat/job_management/modules/candidate/useCase/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/rocketseat/job_management/modules/candidate/useCase/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,10 @@
 package br.com.rocketseat.job_management.modules.candidate.useCase;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.rocketseat.job_management.exceptions.CandidateNotFoundException;
+import br.com.rocketseat.job_management.exceptions.JobNotFoundException;
+import br.com.rocketseat.job_management.modules.candidate.CandidateEntity;
 import br.com.rocketseat.job_management.modules.candidate.CandidateRepository;
 import br.com.rocketseat.job_management.modules.company.repositories.JobRepository;
 
@@ -33,5 +39,21 @@ public class ApplyJobCandidateUseCaseTest {
     } catch (Exception e) {
       assertInstanceOf(CandidateNotFoundException.class, e);
     }
+  }
+
+  @Test
+  @DisplayName("Should not be apply jon with job not found")
+  public void should_not_be_apply_jon_with_job_not_found(){
+    UUID idCandidate = UUID.randomUUID();
+    CandidateEntity candidate = new CandidateEntity();
+    candidate.setId(idCandidate);
+    when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+    try {
+      applyJobCandidateUseCase.execute(idCandidate, null);
+    } catch (Exception e) {
+      assertInstanceOf(JobNotFoundException.class, e);
+    }
+
   }
 }
