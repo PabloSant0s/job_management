92173ae73ec4baa332cdc3584b39f541e662b0b1
Pablo Santos <120131370+PabloSant0s@users.noreply.github.com>
2025-03-11 21:53:24 -0300

    feat: Criando teste de sucesso do apply job

--- src/main/java/br/com/rocketseat/job_management/modules/candidate/useCase/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/rocketseat/job_management/modules/candidate/useCase/ApplyJobCandidateUseCase.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import br.com.rocketseat.job_management.exceptions.CandidateNotFoundException;
 import br.com.rocketseat.job_management.exceptions.JobNotFoundException;
+import br.com.rocketseat.job_management.modules.candidate.entities.ApplyJobEntity;
+import br.com.rocketseat.job_management.modules.candidate.repositories.ApplyJobRepository;
 import br.com.rocketseat.job_management.modules.candidate.repositories.CandidateRepository;
 import br.com.rocketseat.job_management.modules.company.repositories.JobRepository;
 
@@ -17,7 +19,11 @@ public class ApplyJobCandidateUseCase {
 
   @Autowired
   private JobRepository jobRepository;
-  public void execute(UUID idCandidate, UUID idJob){
+
+  @Autowired
+  private ApplyJobRepository applyJobRepository;
+
+  public ApplyJobEntity execute(UUID idCandidate, UUID idJob) {
     candidateRepository.findById(idCandidate).orElseThrow(() -> {
       throw new CandidateNotFoundException();
     });
@@ -25,5 +31,12 @@ public class ApplyJobCandidateUseCase {
     jobRepository.findById(idJob).orElseThrow(() -> {
       throw new JobNotFoundException();
     });
+    ApplyJobEntity applyJobEntity = ApplyJobEntity.builder()
+        .jobId(idJob)
+        .candidateId(idCandidate)
+        .build();
+
+    applyJobEntity = applyJobRepository.save(applyJobEntity);
+    return applyJobEntity;
   }
 }
--- src/test/java/br/com/rocketseat/job_management/modules/candidate/useCase/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/rocketseat/job_management/modules/candidate/useCase/ApplyJobCandidateUseCaseTest.java
@@ -1,11 +1,13 @@
 package br.com.rocketseat.job_management.modules.candidate.useCase;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 import java.util.UUID;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,8 +17,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.rocketseat.job_management.exceptions.CandidateNotFoundException;
 import br.com.rocketseat.job_management.exceptions.JobNotFoundException;
+import br.com.rocketseat.job_management.modules.candidate.entities.ApplyJobEntity;
 import br.com.rocketseat.job_management.modules.candidate.entities.CandidateEntity;
+import br.com.rocketseat.job_management.modules.candidate.repositories.ApplyJobRepository;
 import br.com.rocketseat.job_management.modules.candidate.repositories.CandidateRepository;
+import br.com.rocketseat.job_management.modules.company.entities.JobEntity;
 import br.com.rocketseat.job_management.modules.company.repositories.JobRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,9 +36,12 @@ public class ApplyJobCandidateUseCaseTest {
   @Mock
   private JobRepository jobRepository;
 
+  @Mock
+  private ApplyJobRepository applyJobRepository;
+
   @Test
   @DisplayName("Should not be apply jon with candidate not found")
-  public void should_not_be_apply_jon_with_candidate_not_found(){
+  public void should_not_be_apply_jon_with_candidate_not_found() {
     try {
       applyJobCandidateUseCase.execute(null, null);
     } catch (Exception e) {
@@ -43,7 +51,7 @@ public class ApplyJobCandidateUseCaseTest {
 
   @Test
   @DisplayName("Should not be apply jon with job not found")
-  public void should_not_be_apply_jon_with_job_not_found(){
+  public void should_not_be_apply_jon_with_job_not_found() {
     UUID idCandidate = UUID.randomUUID();
     CandidateEntity candidate = new CandidateEntity();
     candidate.setId(idCandidate);
@@ -54,6 +62,33 @@ public class ApplyJobCandidateUseCaseTest {
     } catch (Exception e) {
       assertInstanceOf(JobNotFoundException.class, e);
     }
+  }
 
+  @Test
+  @DisplayName("Should be able to create a new apply job")
+  public void should_be_able_to_create_a_new_apply_job() {
+    UUID idCandidate = UUID.randomUUID();
+    UUID idJob = UUID.randomUUID();
+
+    when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(new CandidateEntity()));
+    when(jobRepository.findById(idJob)).thenReturn(Optional.of(new JobEntity()));
+
+    ApplyJobEntity applyJobEntity = ApplyJobEntity.builder()
+        .candidateId(idCandidate)
+        .jobId(idJob)
+        .build();
+
+    ApplyJobEntity applyJobEntityCreated = ApplyJobEntity.builder()
+        .candidateId(idCandidate)
+        .jobId(idJob)
+        .id(UUID.randomUUID())
+        .build();
+
+    when(applyJobRepository.save(applyJobEntity)).thenReturn(applyJobEntityCreated);
+
+    var result = applyJobCandidateUseCase.execute(idCandidate, idJob);
+
+    Assertions.assertThat(result).hasFieldOrProperty("id");
+    assertNotNull(result.getId());
   }
 }
